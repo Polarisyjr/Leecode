df748f783f53e31de4a2307036fc9fc4bcd3e511
PolarisBella <yjrcs@umich.edu>
2024-10-29 00:07:19 -0400

    9

--- Top150/274_H-Index.cpp
+++ Top150/274_H-Index.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-        sort(citations);
-        int i=citations.size()-1;
+        std::sort(citations.begin(), citations.end());
         int cnt=0;
         for(int i=citations.size()-1; i>=0; i--){
+            //std::cout<<citations[i]<<std::endl;
             if(citations[i]>cnt){
                 cnt++;
             }
-            else return i;
+            else{
+                break;
+            }
         }
+        return cnt;
     }
 };
